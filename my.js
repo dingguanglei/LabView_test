1eae644f33c59a98a55035dae2e11af02656e7b8
dingguanglei <32232377+dingguanglei@users.noreply.github.com>
2018-01-12 18:26:49 +0800

    选择随机数个数

--- my.js
+++ my.js
@@ -1,60 +1,91 @@
-
 function gen_btn() {
+    "use strict";
     // var seed = "1234";
-    var seed = document.getElementById("seedInput").value+"";
-    if(seed.length!==4){
+    var seed = document.getElementById("seedInput").value + "";
+
+
+    if (seed.length !== 4) {
         alert("请输入4位随机数种子！");
         return;
     }
-    else{
-        var s1 = parseInt(seed[0])+1;
-        var s2 = parseInt(seed[1])+s1;
-        var s3 = parseInt(seed[2])+3;
-        var s4 = parseInt(seed[3])+s2;
+    else {
+        const PRIME1 = [2, 23, 3, 17, 5, 11, 13, 31, 7];
+        const PRIME2 = [229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293];
+
+        var s1 = parseInt(seed[0]) + 1;
+        var s2 = parseInt(seed[1]) + s1;
+        var s3 = parseInt(seed[2]) + 3;
+        var s4 = parseInt(seed[3]) + s2;
         var s5 = 0;
-        var send ="";
-        for (var i = 0; i < 10000; i++) {
+        var send = "";
+        var num = 100000;
+        num = parseInt(document.getElementById("numInput").value);
+        for (var i = 0; i < num; i++) {
 
 
-            s5 = (s1*s3+s2*Math.pow(s4,s1))%(i+13579);
+            s5 = (s1 * s3 + s2 * Math.pow(s4, s1)) % (i + 13579 + PRIME2[i % PRIME2.length]);
 
-            s5 = (s5+"");
-            s5 = parseInt(s5[(i+17)%s5.length])+parseInt((s2+"")[0]);
+            s5 = (s5 + "");
+            s5 = parseInt(s5[(i + PRIME1[i % PRIME1.length]) % s5.length]) + parseInt((s2 + "")[0]);
 
-            send+= (s5+"")[(i+133)%(s5+"").length];
-            s1=s2;
-            s2=s3;
-            s3=s4;
-            s4=s5;
+
+            send += (s5 + "")[(i + PRIME2[i % PRIME2.length]) % (s5 + "").length];
+            s1 = s2;
+            s2 = s3;
+            s3 = s4;
+            s4 = s5;
+        }
+    }
+    var iBeginPos = 0, iEndPos = send.length;
+    for (var i = 0; i < send.length; i = i + 2) {
+        var item = parseInt(send[i]);
+        item = (item+parseInt(seed[3])*7)%10;
+
+
+        var sFrontPart = send.substr(iBeginPos, i);
+        var sTailPart = send.substr(i + 1,iEndPos);
+        send = sFrontPart + item + sTailPart;
     }
 
 
-    }
+
+    // for (var i = 0; i < send.length; i = i + 1) {
     //
-    // var send ="";
-    // for (var i = 0; i < 10000; i++) {
-    //     send+= Math.floor(Math.random() * 10);
+    //     var y = parseInt(send[i]);
+    //     y = y ^ (y >> 11);                 //y右移11个bit
+    //     y = y ^ ((y << 7) & 2636928640);   //y左移7个bit与2636928640相与，再与y进行异或
+    //     y = y ^ ((y << 15) & 4022730752);  //y左移15个bit与4022730752相与，再与y进行异或
+    //     y = y ^ (y >> 18);                 //y右移18个bit再与y进行异或
+    //     y = y % 10;
+    //     var sFrontPart = send.substr(iBeginPos, i);
+    //     var sTailPart = send.substr(i + 1,iEndPos);
+    //     send = sFrontPart + y + sTailPart;
+    //
     // }
+
+
     var sendText = document.getElementById("sendText");
     sendText.value = send;
-
     var send_num = document.getElementById("send_num");
-    send_num.innerHTML= send.length;
+    send_num.innerHTML = send.length;
 
 }
+
 function clear_btn() {
     var receiveText = document.getElementById("receiveText");
     receiveText.value = "";
     var receive_num = document.getElementById("receive_num");
-    receive_num.innerHTML= 0;
+    receive_num.innerHTML = 0;
 
 }
+
 function iGetInnerText(testStr) {
     var resultStr = testStr.replace(/\ +/g, ""); //去掉空格
     resultStr = resultStr.replace(/[ ]/g, "");    //去掉空格
     resultStr = resultStr.replace(/[\r\n]/g, ""); //去掉回车换行
     return resultStr;
 }
+
 function eval_btn() {
 
     var sendText = document.getElementById("sendText").value;
@@ -63,25 +94,24 @@ function eval_btn() {
     var correctNum = 0;
     var Total = sendText.length;
     var send_num = document.getElementById("send_num");
-    send_num.innerHTML= sendText.length;
+    send_num.innerHTML = sendText.length;
     var receive_num = document.getElementById("receive_num");
-    receive_num.innerHTML= receiveText.length;
-    if(!sendText)
-    {
+    receive_num.innerHTML = receiveText.length;
+    if (!sendText) {
         alert("请生成发送字符串!");
         return 0;
     }
-    if(!receiveText){
+    if (!receiveText) {
         alert("请输入接收字符串");
         return 0;
     }
-    if(sendText.length!==receiveText.length){
+    if (sendText.length !== receiveText.length) {
         alert("收、发字符串长度不相等！");
         return 0;
     }
 
-    for(var i=0 ;i<sendText.length;i++){
-        if(sendText[i]===receiveText[i]){
+    for (var i = 0; i < sendText.length; i++) {
+        if (sendText[i] === receiveText[i]) {
             correctNum++;
         }
     }
@@ -89,10 +119,11 @@ function eval_btn() {
     var correct_num = document.getElementById("correct_num");
     var error_num = document.getElementById("error_num");
     var correct_rate = document.getElementById("correct_rate");
-    alert(correctNum);
+
     total_num.innerHTML = Total;
     correct_num.innerHTML = correctNum;
-    error_num.innerHTML = parseInt(Total)-parseInt(correctNum);
-    correct_rate.innerHTML = (parseFloat(correctNum)/parseFloat(Total)).toFixed(4)*100+"%";
+    error_num.innerHTML = parseInt(Total) - parseInt(correctNum);
+    correct_rate.innerHTML = (parseFloat(correctNum) / parseFloat(Total)).toFixed(4) * 100 + "%";
+    alert("正确率：" + correct_rate.innerHTML);
 }
 
